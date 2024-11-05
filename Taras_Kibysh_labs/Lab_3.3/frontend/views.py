72fef44c2d5f608ae95e92fac7cdd337430cbf0f
Kibysh Taras <taras.kibysh,shi.2023@lpnu.ua>
2024-11-05 23:03:47 +0200

    refactored version

--- Taras_Kibysh_labs/Lab_3.3/frontend/views.py
+++ Taras_Kibysh_labs/Lab_3.3/frontend/views.py
@@ -2,25 +2,12 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
 import requests
 
-from openid.extensions.draft.pape5 import Request
 
 from .forms import CustomerForm
 
 # Create your views here.
 
 base_url = "http://127.0.0.1:8000/"
-
-# def user_list_view(request):
-#     url = f"{base_url}user/"  # Вкажіть правильний URL для API
-#     response = requests.get(url)
-#
-#     # Перевірка статусу відповіді
-#     if response.status_code == 200:
-#         users = response.json()  # Отримати дані користувачів
-#     else:
-#         users = []  # Якщо щось пішло не так, відправимо порожній список
-#
-#     return render(request, 'frontend/list.html', {'users': users})  #
 
 
 def user_list_view(request):
@@ -93,36 +80,6 @@ def create_user(request):
     print("hello world")
     return render(request, 'frontend/create_form.html', context)
 
-
-
-# def change_user(request, id):
-#     url = f"{base_url}user/{id}/"
-#     form = CustomerForm()
-#
-#     response = requests.get(url, cookies=request.COOKIES)  # Використовуємо кукі для аутентифікації
-#
-#     if response.status_code == 200:
-#         user = response.json()
-#     else:
-#         user = []
-#         print(f"Помилка API: Статус {response.status_code}")
-#
-#
-#     # if request.method == 'POST':
-#     #         form = CustomerForm(request.POST)
-#     #         if form.is_valid():
-#     #             cleaned_data = form.cleaned_data
-#     #             cleaned_data['gender'] = cleaned_data['gender'].id
-#     #             put_response = requests.put(url, data=cleaned_data, cookies=request.COOKIES)
-#     #
-#     #             if put_response.status_code == 200:  # If successfully updated
-#     #                 return redirect('get_user', id)
-#     #             else:
-#     #                 form.add_error(None, "Error updating user via API")
-#     #         else:
-#     #             form.add_error(None, "Form data is invalid. Please check your input.")
-#
-#     return render(request, 'frontend/change_form.html', {'user': user})
 
 
 def change_user(request, id):
